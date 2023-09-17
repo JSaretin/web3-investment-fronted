fc33b88bb20f344a69e38941ee49817470a628b4
JSaretin <jsaretin@gmail.com>
2023-09-17 17:26:52 +0100

    calculate reward

--- src/routes/invest/Calculator.svelte
+++ src/routes/invest/Calculator.svelte
@@ -9,33 +9,37 @@
 
 	export let plans: Plan[];
 
-	let selectedPlan: number = plans.length > 0 ? 1 : 0;
-	let investment: number = plans.filter((p) => p.id == selectedPlan)[0]?.minDeposit || 0;
-	let reward: number = 0;
+	let selectedPlan: number;
+	let investment: number;
+	let reward: string = '0.0';
 	let duration: string;
 
+	let loaded: boolean;
+
+	$: if (!loaded && !$contractInstance != undefined) {
+		loaded = true;
+		selectedPlan = plans.length > 0 ? plans[0].id : 0;
+		investment = plans.filter((p) => p.id == selectedPlan)[0]?.minDeposit || 0;
+	}
+
 	async function calculateReward() {
+		if (!investment || investment <= 0) return;
+		if (!duration) return;
+
 		const now = new Date();
 		const future = new Date(duration);
-		const diff = future.getTime() - now.getTime();
+		const diff = parseInt(((future.getTime() - now.getTime()) / 1000).toString());
 		if (diff <= 0) return;
+		const planPercent = plans.filter((p) => p.id == selectedPlan)[0]?.percent | 0;
 
-		reward = Number(
-			$web3.utils.fromWei(
-				await $contractInstance.calculateReward(
-					investment,
-					plans.filter((p) => p.id == selectedPlan)[0]?.percent,
-					diff
-				),
-				'ether'
-			)
-		);
+		const result = await $contractInstance.calculateReward(investment, planPercent, diff);
+		reward = Number($web3.utils.fromWei(result, 'ether')).toFixed(5);
 	}
 </script>
 
 <div class="flex flex-col max-w-sm gap-2 w-full rounded-lg p-6 shadow-sm shadow-gray-950">
 	<p>
-		Invest <b>{investment} ETH</b> and get <b>{investment + reward}</b> after {reward} days
+		Invest <b>{investment} ETH</b> and get <b>{reward}</b> after {reward} days
 	</p>
 	<label class="flex flex-col">
 		Investment
@@ -47,15 +51,15 @@
 			on:input={calculateReward}
 		/>
 	</label>
-	<label class="flex flex-col">
+	<label class="flex flex-col" on:input={calculateReward}>
 		Days
-		<input type="datetime" class="bg-gray-200 p-1 rounded" bind:value={duration} />
+		<input type="date" bind:value={duration} />
 	</label>
 	<label class="flex flex-col">
 		Select Plan
 		<select class="p-2 rounded-md" bind:value={selectedPlan} on:change={calculateReward}>
 			{#each plans as plan (plan.id)}
-				<option value={plan.id}>Plan #{plan.id}</option>
+				<option value={plan.id} selected={selectedPlan == plan.id}>Plan #{plan.id}</option>
 			{/each}
 		</select>
 	</label>
