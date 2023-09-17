e1abda76fc6770f2c72539481aff2407a054dce3
JSaretin <jsaretin@gmail.com>
2023-09-17 13:20:41 +0100

    get plan ID from smart contract

--- src/routes/invest/Plan.svelte
+++ src/routes/invest/Plan.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { web3 } from 'svelte-web3';
 	import { createEventDispatcher } from 'svelte';
 
 	export let plan: {
 		percent: number;
 		minDeposit: number;
+		id: number;
 	};
-	export let id: number;
 	export let expand: boolean;
 
 	const emiter = createEventDispatcher();
@@ -16,7 +17,7 @@
 	<button class="w-full p-2 flex flex-col gap-2" on:click={() => emiter('select')}>
 		<div class="font-bold w-full flex justify-between place-items-center align-middle">
 			<h3 class="">Plan</h3>
-			#{id + 1}
+			#{plan.id}
 		</div>
 		<div class="flex w-full justify-between">
 			<p>yearly ROI:</p>
@@ -28,7 +29,7 @@
 		</div>
 		<div class="flex w-full justify-between">
 			<p>min deposit:</p>
-			<p>{plan.minDeposit}</p>
+			<p>{$web3.utils.fromWei(plan.minDeposit, 'ether')} ETH</p>
 		</div>
 	</button>
 
@@ -39,12 +40,14 @@
 				<input
 					type="text"
 					inputmode="decimal"
-					class="p-2 rounded bg-gray-300 text-gray-950"
+					class="{'p-2 rounded bg-gray-300 text-gray-950 outline-2 border-none ' +
+						(!investment || investment <= 0 ? 'outline-pink-400' : 'outline-green-400')}}"
 					bind:value={investment}
 				/>
 			</label>
 			<button
-				class="py-1 px-6 bg-inherit hover:bg-gray-950 text-gray-950 font-bold border border-gray-950 hover:text-gray-50 rounded"
+				disabled={!investment || investment <= 0}
+				class="py-1 px-6 bg-inherit hover:bg-gray-950 text-gray-950 font-bold border border-gray-950 hover:text-gray-50 rounded disabled:cursor-not-allowed"
 				on:click={() => {
 					if (!Boolean(investment)) return;
 					emiter('invest', investment);
